732ad5cc3f5d57c629ef92a5f2c657b28dd7dbaf
david <DavidvanWuykhuyse@gmail.com>
2024-09-15 14:31:47 +0200

    implement: main.py
    prints a numpy array

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+if __name__ == "__main__":
+    number_list = []
+    for number in range(0,11):
+        number_list.append(number)
+
+    numpy_array = np.array = (number_list)
+    print(numpy_array)
