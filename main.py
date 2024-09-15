d654df58ac45e3d593cbd844339bbe08a71686f7
david <DavidvanWuykhuyse@gmail.com>
2024-09-15 16:56:20 +0200

    add: style changes
    fixes some style mistakes

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import numpy as np
 
 if __name__ == "__main__":
     number_list = []
-    for number in range(0,11):
+    for number in range(1, 11):
         number_list.append(number)
 
-    numpy_array = np.array = (number_list)
+    numpy_array = np.array(number_list)
     print(numpy_array)
-    
